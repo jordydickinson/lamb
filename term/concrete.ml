342a56b5a11cb2af89d4af5cadcb0e064ad0ba80
Jordy Dickinson <jordy.dickinson@icloud.com>
2021-11-03 14:44:29 -0400

    Annotate concrete syntax with tags

--- term/concrete.ml
+++ term/concrete.ml
@@ -1,6 +1,6 @@
-type (+'atom, +'ann) t =
-| Local of Ident.t
-| Global of Global.indexed
-| Atom of 'atom
-| Abs of (Ident.t * 'ann option) list * ('atom, 'ann) t
-| App of ('atom, 'ann) t * ('atom, 'ann) t list
+type (+'atom, +'ann, +'tag) t =
+| Local of 'tag option * Ident.t
+| Global of 'tag option * Global.indexed
+| Atom of 'tag option * 'atom
+| Abs of 'tag option * (Ident.t * 'ann option) list * ('atom, 'ann, 'tag) t
+| App of 'tag option * ('atom, 'ann, 'tag) t * ('atom, 'ann, 'tag) t list
