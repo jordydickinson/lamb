0c45a3cee9f27af488dc39cd4e4c221e0700100a
Jordy Dickinson <jordy.dickinson@icloud.com>
2021-11-03 14:54:56 -0400

    Add Concrete.replace_tags

--- term/concrete.ml
+++ term/concrete.ml
@@ -4,3 +4,17 @@ type (+'atom, +'ann, +'tag) t =
 | Atom of 'tag option * 'atom
 | Abs of 'tag option * (Ident.t * 'ann option) list * ('atom, 'ann, 'tag) t
 | App of 'tag option * ('atom, 'ann, 'tag) t * ('atom, 'ann, 'tag) t list
+
+let rec replace_tags tags = function
+| Local (_, id) -> Local (Tags.find_opt Path.here tags, id)
+| Global (_, x) -> Global (Tags.find_opt Path.here tags, x)
+| Atom (_, a) -> Atom (Tags.find_opt Path.here tags, a)
+| Abs (_, xs, body) ->
+  let body = replace_tags (Tags.goto Path.(body here) tags) body in
+  Abs (Tags.find_opt Path.here tags, xs, body)
+| App (_, f, vs) ->
+  let f = replace_tags (Tags.goto Path.(head here) tags) f in
+  let vs = vs
+  |> List.mapi (fun i v -> replace_tags (Tags.goto Path.(spine i here) tags) v)
+  in
+  App (Tags.find_opt Path.here tags, f, vs)
