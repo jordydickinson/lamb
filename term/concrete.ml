8a947cb451059347d91c8bd495bdc06192b7ac58
Jordy Dickinson <jordy.dickinson@icloud.com>
2021-11-03 15:12:49 -0400

    Add Concrete.untag

--- term/concrete.ml
+++ term/concrete.ml
@@ -5,6 +5,8 @@ type (+'atom, +'ann, +'tag) t =
 | Abs of 'tag option * (Ident.t * 'ann option) list * ('atom, 'ann, 'tag) t
 | App of 'tag option * ('atom, 'ann, 'tag) t * ('atom, 'ann, 'tag) t list
 
+type (+'atom, +'ann) untagged = ('atom, 'ann, void) t
+
 let rec replace_tags tags = function
 | Local (_, id) -> Local (Tags.find_opt Path.here tags, id)
 | Global (_, x) -> Global (Tags.find_opt Path.here tags, x)
@@ -18,3 +20,10 @@ let rec replace_tags tags = function
   |> List.mapi (fun i v -> replace_tags (Tags.goto Path.(spine i here) tags) v)
   in
   App (Tags.find_opt Path.here tags, f, vs)
+
+let rec untag : _ -> _ untagged = function
+| Local (_, id) -> Local (None, id)
+| Global (_, x) -> Global (None, x)
+| Atom (_, a) -> Atom (None, a)
+| Abs (_, xs, body) -> Abs (None, xs, untag body)
+| App (_, f, vs) -> App (None, untag f, List.map untag vs)
