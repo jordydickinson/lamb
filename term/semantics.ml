e98c148fa87a343bef5cb37d89f397fa6dca3141
Jordy Dickinson <jordy.dickinson@icloud.com>
2021-11-17 15:18:16 -0500

    Allow specialization of Semantics with Atoms

--- term/semantics.ml
+++ term/semantics.ml
@@ -2,193 +2,194 @@ module Index = Local.Index
 module Level = Local.Level
 module Pool = Local.Pool
 
-type (+'atom, +'ann) t =
-| Local of Local.level
-| Global of Global.leveled
-| Atom of 'atom
-| Abs of ('atom, 'ann) clos
-| App of ('atom, 'ann) t * ('atom, 'ann) t list
+include Semantics_intf
 
-and (+'atom, +'ann) ctx = (('atom, 'ann) t, ('atom, 'ann) t, 'ann) Ctx.t
+module Common = struct
 
-and (+'atom, +'ann) clos =
-  { ctx: ('atom, 'ann) ctx
-  ; bound: 'ann option list
-  ; body: ('atom, 'ann) Syntax.t
-  }
+  let next_neutral pool = Local (Pool.next_level pool)
 
-type (+'atom, +'ann) vars = (('atom, 'ann) t, ('atom, 'ann) t) Vars.t
+  let add_neutral ?ann ctx =
+    let v = next_neutral @@ Locals.pool @@ Ctx.locals ctx in
+    Ctx.add ?data:ann v ctx
 
-let next_neutral pool = Local (Pool.next_level pool)
+  let add_neutrals anns ctx =
+    List.fold_left (fun ctx ann -> add_neutral ?ann ctx) ctx anns
 
-let add_neutral ?ann ctx =
-  let v = next_neutral @@ Locals.pool @@ Ctx.locals ctx in
-  Ctx.add ?data:ann v ctx
+  let neutral_ctx pool =
+    let rec neutral_ctx' ctx =
+      if Pool.equal pool (Locals.pool @@ Ctx.locals ctx)
+      then ctx
+      else neutral_ctx' (add_neutral ctx)
+    in
+    neutral_ctx' @@ Ctx.make Globals.empty
 
-let add_neutrals anns ctx =
-  List.fold_left (fun ctx ann -> add_neutral ?ann ctx) ctx anns
+  let rec spec ctx : _ Syntax.t -> _ t = function
+  | Local i -> Locals.ith i @@ Ctx.locals ctx
+  | Global x -> Globals.find x @@ Ctx.globals ctx
+  | Atom a -> Atom a
+  | Abs (bound, body) -> Abs { ctx; bound; body }
+  | App (f, vs) -> App (spec ctx f, List.map (spec ctx) vs)
 
-let neutral_ctx pool =
-  let rec neutral_ctx' ctx =
-    if Pool.equal pool (Locals.pool @@ Ctx.locals ctx)
-    then ctx
-    else neutral_ctx' (add_neutral ctx)
-  in
-  neutral_ctx' @@ Ctx.make Globals.empty
+  let fix pool = spec @@ neutral_ctx pool
 
-let rec spec ctx : _ Syntax.t -> _ t = function
-| Local i -> Locals.ith i @@ Ctx.locals ctx
-| Global x -> Globals.find x @@ Ctx.globals ctx
-| Atom a -> Atom a
-| Abs (bound, body) -> Abs { ctx; bound; body }
-| App (f, vs) -> App (spec ctx f, List.map (spec ctx) vs)
+  let open_clos clos =
+    let ctx = add_neutrals clos.bound clos.ctx in
+    spec ctx clos.body, ctx
 
-let fix pool = spec @@ neutral_ctx pool
+  let rec gen pool : _ t -> _ Syntax.t = function
+  | Local l -> Local (Pool.index_of_level pool l)
+  | Global x -> Global x
+  | Atom a -> Atom a
+  | Abs clos ->
+    let body, ctx = open_clos clos in
+    Abs (clos.bound, gen ctx.scope.pool body)
+  | App (f, vs) -> App (gen pool f, List.map (gen pool) vs)
 
-let open_clos clos =
-  let ctx = add_neutrals clos.bound clos.ctx in
-  spec ctx clos.body, ctx
+  let rec replace vars : _ t -> _ t = function
+  | Local l as e ->
+    begin match Locals.lth_opt l @@ Vars.locals vars with
+    | None -> e
+    | Some v -> v
+    end
+  | Global x as e ->
+    begin match Globals.find_opt x @@ Vars.globals vars with
+    | None -> e
+    | Some v -> v
+    end
+  | Atom _ as e -> e
+  | Abs clos as e ->
+    let ctx = Ctx.map_locals (replace vars) clos.ctx in
+    if ctx.vars.locals == clos.ctx.vars.locals then e else
+    Abs { clos with ctx }
+  | App (f, vs) as e ->
+    let f' = replace vars f and vs' = List.map (replace vars) vs in
+    if f == f' && vs == vs' then e else App (f', vs')
+end
+include Common
 
-let rec gen pool : _ t -> _ Syntax.t = function
-| Local l -> Local (Pool.index_of_level pool l)
-| Global x -> Global x
-| Atom a -> Atom a
-| Abs clos ->
-  let body, ctx = open_clos clos in
-  Abs (clos.bound, gen ctx.scope.pool body)
-| App (f, vs) -> App (gen pool f, List.map (gen pool) vs)
+module Make (Atom: Atom) = struct
+  include Common
 
-let rec replace vars : _ t -> _ t = function
-| Local l as e ->
-  begin match Locals.lth_opt l @@ Vars.locals vars with
-  | None -> e
-  | Some v -> v
-  end
-| Global x as e ->
-  begin match Globals.find_opt x @@ Vars.globals vars with
-  | None -> e
-  | Some v -> v
-  end
-| Atom _ as e -> e
-| Abs clos as e ->
-  let ctx = Ctx.map_locals (replace vars) clos.ctx in
-  if ctx.vars.locals == clos.ctx.vars.locals then e else
-  Abs { clos with ctx }
-| App (f, vs) as e ->
-  let f' = replace vars f and vs' = List.map (replace vars) vs in
-  if f == f' && vs == vs' then e else App (f', vs')
+  type atom = Atom.t
 
-let beta_app f vs = match f with
-| Abs { ctx; bound; body } ->
-  let bvs, rem = List.combine_rem bound vs in
-  let ctx = List.fold_left (fun ctx (ann, v) -> Ctx.add ?data:ann v ctx) ctx bvs in
-  begin match rem with
-  | None -> Some (spec ctx body)
-  | Some Left bound -> Some (Abs { ctx; bound; body })
-  | Some Right vs ->
-    let f = spec ctx body in
-    Some (App (f, vs))
-  end
-| _ -> None
+  type nonrec +'ann t = (atom, 'ann) t
+  type nonrec +'ann ctx = (atom, 'ann) ctx
+  type nonrec +'ann clos = (atom, 'ann) clos
+  type nonrec +'ann vars = (atom, 'ann) vars
 
-let beta = function
-| App (f, vs) -> beta_app f vs
-| _ -> None
+  let beta_app f vs = match f with
+  | Abs { ctx; bound; body } ->
+    let bvs, rem = List.combine_rem bound vs in
+    let ctx = List.fold_left (fun ctx (ann, v) -> Ctx.add ?data:ann v ctx) ctx bvs in
+    begin match rem with
+    | None -> Some (spec ctx body)
+    | Some Left bound -> Some (Abs { ctx; bound; body })
+    | Some Right vs ->
+      let f = spec ctx body in
+      Some (App (f, vs))
+    end
+  | Atom a -> Atom.beta_app a vs
+  | _ -> None
 
-let under_clos f clos =
-  let body, ctx = open_clos clos in
-  let body = f body |> gen ctx.scope.pool in
-  { clos with body }
+  let beta = function
+  | App (f, vs) -> beta_app f vs
+  | _ -> None
 
-let rec cbn = function
-| Local _ | Global _ | Atom _ | Abs _ as e -> e
-| App (f, vs) as e ->
-  let f' = cbn f in
-  begin match beta_app f' vs with
-  | None when f == f' -> e
-  | None -> App (f', vs)
-  | Some v -> cbn v
-  end
+  let under_clos f clos =
+    let body, ctx = open_clos clos in
+    let body = f body |> gen ctx.scope.pool in
+    { clos with body }
 
-let rec cbv = function
-| Local _ | Global _ | Atom _ | Abs _ as e -> e
-| App (f, vs) as e ->
-  let f' = cbv f in
-  let vs' = List.map cbv vs in
-  begin match beta_app f vs with
-  | None when f == f' && vs == vs' -> e
-  | None -> App (f', vs')
-  | Some v -> cbv v
-  end
+  let rec cbn = function
+  | Local _ | Global _ | Atom _ | Abs _ as e -> e
+  | App (f, vs) as e ->
+    let f' = cbn f in
+    begin match beta_app f' vs with
+    | None when f == f' -> e
+    | None -> App (f', vs)
+    | Some v -> cbn v
+    end
 
-let rec hsr = function
-| Local _ | Global _ | Atom _ as e -> e
-| Abs clos as e ->
-  let clos' = under_clos hsr clos in
-  if clos == clos' then e else Abs clos'
-| App (f, vs) as e ->
-  let f' = hsr f in
-  begin match beta_app f' vs with
-  | None when f == f' -> e
-  | None -> App (f', vs)
-  | Some v -> hsr v
-  end
+  let rec cbv = function
+  | Local _ | Global _ | Atom _ | Abs _ as e -> e
+  | App (f, vs) as e ->
+    let f' = cbv f in
+    let vs' = List.map cbv vs in
+    begin match beta_app f vs with
+    | None when f == f' && vs == vs' -> e
+    | None -> App (f', vs')
+    | Some v -> cbv v
+    end
 
-let rec nor = function
-| Local _ | Global _ | Atom _ as e -> e
-| Abs clos as e ->
-  let clos' = under_clos nor clos in
-  if clos == clos' then e else Abs clos'
-| App (f, vs) as e ->
-  let f' = cbn f in
-  begin match beta_app f' vs with
-  | None ->
-    let f' = nor f' and vs' = List.map nor vs in
-    if f == f' && vs == vs' then e else
-    App (f', vs')
-  | Some v -> nor v
-  end
+  let rec hsr = function
+  | Local _ | Global _ | Atom _ as e -> e
+  | Abs clos as e ->
+    let clos' = under_clos hsr clos in
+    if clos == clos' then e else Abs clos'
+  | App (f, vs) as e ->
+    let f' = hsr f in
+    begin match beta_app f' vs with
+    | None when f == f' -> e
+    | None -> App (f', vs)
+    | Some v -> hsr v
+    end
 
-let rec hao = function
-| Local _ | Global _ | Atom _ as e -> e
-| Abs clos as e ->
-  let clos' = under_clos hao clos in
-  if clos == clos' then e else Abs clos'
-| App (f, vs) as e ->
-  let f' = cbv f and vs' = List.map hao vs in
-  begin match beta_app f' vs' with
-  | None ->
-    let f' = hao f' in
-    if f == f' && vs == vs' then e else
-    App (f', vs')
-  | Some v -> hao v
-  end
+  let rec nor = function
+  | Local _ | Global _ | Atom _ as e -> e
+  | Abs clos as e ->
+    let clos' = under_clos nor clos in
+    if clos == clos' then e else Abs clos'
+  | App (f, vs) as e ->
+    let f' = cbn f in
+    begin match beta_app f' vs with
+    | None ->
+      let f' = nor f' and vs' = List.map nor vs in
+      if f == f' && vs == vs' then e else
+      App (f', vs')
+    | Some v -> nor v
+    end
 
-let rec hno = function
-| Local _ | Global _ | Atom _ as e -> e
-| Abs clos as e ->
-  let clos' = under_clos hno clos in
-  if clos == clos' then e else Abs clos'
-| App (f, vs) as e ->
-  let f' = hsr f in
-  begin match beta_app f' vs with
-  | None ->
-    let f' = hno f' and vs' = List.map hno vs in
-    if f == f' && vs == vs' then e else
-    App (f', vs')
-  | Some v -> hno v
-  end
+  let rec hao = function
+  | Local _ | Global _ | Atom _ as e -> e
+  | Abs clos as e ->
+    let clos' = under_clos hao clos in
+    if clos == clos' then e else Abs clos'
+  | App (f, vs) as e ->
+    let f' = cbv f and vs' = List.map hao vs in
+    begin match beta_app f' vs' with
+    | None ->
+      let f' = hao f' in
+      if f == f' && vs == vs' then e else
+      App (f', vs')
+    | Some v -> hao v
+    end
 
-let rec apo = function
-| Local _ | Global _ | Atom _ as e -> e
-| Abs clos as e ->
-  let clos' = under_clos apo clos in
-  if clos == clos' then e else Abs clos'
-| App (f, vs) as e ->
-  let f' = apo f in
-  let vs' = List.map apo vs in
-  begin match beta_app f' vs' with
-  | None when f == f' && vs == vs' -> e
-  | None -> App (f', vs')
-  | Some v -> apo v
-  end
+  let rec hno = function
+  | Local _ | Global _ | Atom _ as e -> e
+  | Abs clos as e ->
+    let clos' = under_clos hno clos in
+    if clos == clos' then e else Abs clos'
+  | App (f, vs) as e ->
+    let f' = hsr f in
+    begin match beta_app f' vs with
+    | None ->
+      let f' = hno f' and vs' = List.map hno vs in
+      if f == f' && vs == vs' then e else
+      App (f', vs')
+    | Some v -> hno v
+    end
+
+  let rec apo = function
+  | Local _ | Global _ | Atom _ as e -> e
+  | Abs clos as e ->
+    let clos' = under_clos apo clos in
+    if clos == clos' then e else Abs clos'
+  | App (f, vs) as e ->
+    let f' = apo f in
+    let vs' = List.map apo vs in
+    begin match beta_app f' vs' with
+    | None when f == f' && vs == vs' -> e
+    | None -> App (f', vs')
+    | Some v -> apo v
+    end
+end
--- term/semantics.mli
+++ term/semantics.mli
@@ -1,34 +1,6 @@
 type pool := Local.pool
 
-(** Semantic terms, parameterized by atoms and annotations.
-
-    This form has levels for both local and global variables, except under
-    closures where local variables are indexed. This makes it very easy to
-    move these terms about in a context since levels require no shifting when
-    the context is extended or when performing substitutions. Additionally,
-    the use of closures for abstractions means that substitutions can be done
-    much more efficiently than would normally be possible. So this is a good
-    form of syntax for performing reductions. To perform an 
-    alpha-beta-equivalence check, the recommended method would be to apply
-    the reduction functions in this form, then convert back to syntax and
-    perform an alpha-equivalence check.
- *)
-type (+'atom, +'ann) t =
-| Local of Local.level
-| Global of Global.leveled
-| Atom of 'atom
-| Abs of ('atom, 'ann) clos
-| App of ('atom, 'ann) t * ('atom, 'ann) t list
-
-and (+'atom, +'ann) ctx = (('atom, 'ann) t, ('atom, 'ann) t, 'ann) Ctx.t
-
-and (+'atom, +'ann) clos =
-  { ctx: ('atom, 'ann) ctx
-  ; bound: 'ann option list
-  ; body: ('atom, 'ann) Syntax.t
-  }
-
-type (+'atom, +'ann) vars = (('atom, 'ann) t, ('atom, 'ann) t) Vars.t
+include module type of struct include Semantics_intf end
 
 (** [fix pool term] fixes the variables in [term] to produce a semantic term.
     Effectively, this simply uses [pool] to convert the indices of [term] to
@@ -49,51 +21,4 @@ val spec: ('atom, 'ann) ctx -> ('atom, 'ann) Syntax.t -> ('atom, 'ann) t
     substitutions due to previous calls to [replace]. *)
 val gen: pool -> ('atom, 'ann) t -> ('atom, 'ann) Syntax.t
 
-(** [beta_app f vs] is [Some reduct] if [f] applied to the arguments [vs] 
-    reduces, and [None] if it does not. Note that this need not fully reduce
-    the term, it will only reduce a single outer beta redex. *)
-val beta_app: ('atom, 'ann) t -> ('atom, 'ann) t list -> ('atom, 'ann) t option
-
-(** [beta term] is [Some reduct] if [term] is a beta redex, and [None] 
-    otherwise. *)
-val beta: ('atom, 'ann) t -> ('atom, 'ann) t option
-
-(** [cbn term] reduces [term] to weak head normal form using the call-by-name
-    evaluation strategy.  *)
-val cbn: ('atom, 'ann) t -> ('atom, 'ann) t
-
-(** [cbv term] reduces [term] to weak normal form using the call-by-value
-    evaluation strategy. *)
-val cbv: ('atom, 'ann) t -> ('atom, 'ann) t
-
-(** [hsr term] reduces [term] to head normal form using the head-spine 
-    reduction evaluation strategy. *)
-val hsr: ('atom, 'ann) t -> ('atom, 'ann) t
-
-(** [nor term] reduces [term] to normal form using the normal order evaluation
-    strategy. This strategy is normalizing; i.e., if the term has a normal 
-    form, then this evaluation strategy will terminate. *)
-val nor: ('atom, 'ann) t -> ('atom, 'ann) t
-
-(** [hao term] reduces [term] to normal form using the hybrid applicative order
-    evaluation strategy. This strategy is not normalizing; i.e., for some
-    terms which have normal forms, this strategy may loop forever. An example
-    is the Ω ≡ (λx. x x)(λx. x x) fixpoint combinator. However, it is much
-    faster than {!val:nor}, and may be used with fixpoint combinators designed
-    for a strict evaluation strategy: e.g.,
-    Y ≡ λh.(λx.λa.h (x x) a)(λx.λa.h (x x) a). *)
-val hao: ('atom, 'ann) t -> ('atom, 'ann) t
-
-(** [hno term] reduces [term] to normal form using the hybrid normal order
-    evaluation strategy. This strategy is normalizing; i.e., if the term has
-    a normal form, then this evaluation strategy will terminate. *)
-val hno: ('atom, 'ann) t -> ('atom, 'ann) t
-
-(** [apo term] reduces [term] to normal form using the applicative order
-    evaluation strategy. This strategy is not normalizing; i.e., for some
-    terms which have normal forms, this strategy may loop forever. In fact,
-    even fixpoints designed for strict evaluation may fail to normalize.
-    However, it is very efficient, and it may be useful if you know that your
-    terms do not contain such fixpoints. For example, languages which perform
-    totality checking can safely use this evaluation strategy. *)
-val apo: ('atom, 'ann) t -> ('atom, 'ann) t
+module Make (Atom: Atom): S with type atom = Atom.t
