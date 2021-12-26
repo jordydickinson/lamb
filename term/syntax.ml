fecfff797d2e5159f8ddb99ad788964f9a274d31
Jordy Dickinson <jordy.dickinson@icloud.com>
2021-12-26 11:22:56 -0500

    Remove syntax functor

--- term/syntax.ml
+++ term/syntax.ml
@@ -1,49 +1,45 @@
 module Index = Local.Index
 
-include Syntax_intf
+type (+'atom, +'ann, +'tag) t =
+| Local of 'tag option * Local.index
+| Global of 'tag option * Global.leveled
+| Atom of 'tag option * 'atom
+| Abs of 'tag option * 'ann option list * ('atom, 'ann, 'tag) t
+| App of 'tag option * ('atom, 'ann, 'tag) t * ('atom, 'ann, 'tag) t list
 
-module Common = struct
-  let rec fix_concrete vars : _ Concrete.t -> _ = function
-  | Local (tag, id) -> Local (tag, Locals.index (Ident.equal id) (Vars.locals vars))
-  | Global (tag, x) -> Global (tag, Globals.fix x @@ Vars.globals vars)
-  | Atom (tag, a) -> Atom (tag, a)
-  | Abs (tag, [], body) -> Abs (tag, [], fix_concrete vars body) (* Avoids an allocation *)
-  | Abs (tag, bound, body) ->
-    let ids, bound = List.split bound in
-    let body =
-      vars
-      |> Vars.update_locals (fun xs -> List.fold_left (Fun.flip Locals.add) xs ids)
-      |> Fun.flip fix_concrete body
-    in
-    Abs (tag, bound, body)
-  | App (tag, f, vs) ->
-    let f = fix_concrete vars f in
-    let vs = List.map (fix_concrete vars) vs in
-    App (tag, f, vs)
-end
-include Common
+let rec fix_concrete vars : _ Concrete.t -> _ = function
+| Local (tag, id) -> Local (tag, Locals.index (Ident.equal id) (Vars.locals vars))
+| Global (tag, x) -> Global (tag, Globals.fix x @@ Vars.globals vars)
+| Atom (tag, a) -> Atom (tag, a)
+| Abs (tag, [], body) -> Abs (tag, [], fix_concrete vars body) (* Avoids an allocation *)
+| Abs (tag, bound, body) ->
+  let ids, bound = List.split bound in
+  let body =
+    vars
+    |> Vars.update_locals (fun xs -> List.fold_left (Fun.flip Locals.add) xs ids)
+    |> Fun.flip fix_concrete body
+  in
+  Abs (tag, bound, body)
+| App (tag, f, vs) ->
+  let f = fix_concrete vars f in
+  let vs = List.map (fix_concrete vars) vs in
+  App (tag, f, vs)
 
-module Make (Atom: Atom) = struct
-  module rec T: S with type atom = Atom.t = struct
-    include Common
-    
-    type atom = Atom.t
-    type nonrec (+'ann, +'tag) t = (atom, 'ann, 'tag) t
-
-    let rec equal x y = match x, y with
-    | Local (_, i), Local (_, j) -> Index.equal i j
-    | Local _, _ -> false
-    | Global (_, x), Global (_, y) -> Global.equal x y
-    | Global _, _ -> false
-    | Atom (_, a), Atom (_, b) -> Atom.equal (module T) a b
-    | Atom _, _ -> false
-    | Abs (_, xs, body1), Abs (_, ys, body2) ->
-      List.compare_lengths xs ys = 0 && equal body1 body2
-    | Abs _, _ -> false
-    | App (_, f1, vs1), App (_, f2, vs2) ->
-      equal f1 f2 &&
-      List.equal equal vs1 vs2
-    | App _, _ -> false
+let rec equal ?equal_ann equal_atom a b = match a, b with
+| Local (_, i), Local (_, j) -> Local.Index.equal i j
+| Local _, _ -> false
+| Global (_, x), Global (_, y) -> Global.equal x y
+| Global _, _ -> false
+| Atom (_, a), Atom (_, b) -> equal_atom a b
+| Atom _, _ -> false
+| Abs (_, bound, body), Abs (_, bound', body') ->
+  equal ?equal_ann equal_atom body body' &&
+  begin match equal_ann with
+  | None -> List.compare_lengths bound bound' = 0
+  | Some equal_ann -> List.equal (Option.equal equal_ann) bound bound'
   end
-  include T
-end
+| Abs _, _ -> false
+| App (_, f, vs), App (_, f', vs') ->
+  equal ?equal_ann equal_atom f f' &&
+  List.equal (equal ?equal_ann equal_atom) vs vs'
+| App _, _ -> false
--- term/syntax.mli
+++ term/syntax.mli
@@ -1,4 +1,17 @@
-include module type of struct include Syntax_intf end
+(** Syntactic terms, parameterized by atoms, annotations on abstractions, and
+    tags on terms.
+
+    Local variables are indexed, and global variables are leveled. This makes
+    it easy to move these terms around in some ambient global context so long
+    as they're closed. This is also a convenient form of syntax for performing
+    alpha-equivalence checks.
+  *)
+type (+'atom, +'ann, +'tag) t =
+| Local of 'tag option * Local.index
+| Global of 'tag option * Global.leveled
+| Atom of 'tag option * 'atom
+| Abs of 'tag option * 'ann option list * ('atom, 'ann, 'tag) t
+| App of 'tag option * ('atom, 'ann, 'tag) t * ('atom, 'ann, 'tag) t list
 
 (** [fix_concrete vars term] fixes all the global variables in [term] to
   produce a syntactic term. No substitutions are performed and any tags in the
@@ -6,4 +19,7 @@ include module type of struct include Syntax_intf end
   tags will need updating in the returned tags structure. *)
 val fix_concrete: (Ident.t, _) Vars.t -> ('atom, 'ann, 'tag) Concrete.t -> ('atom, 'ann, 'tag) t
 
-module Make (Atom: Atom): S with type atom := Atom.t
+val equal: ?equal_ann:('ann -> 'ann -> bool)
+  -> ('atom -> 'atom -> bool)
+  -> ('atom, 'ann, 'tag) t -> ('atom, 'ann, 'tag) t
+  -> bool
